4bd89f7a5a70196b23093274375d62e962bf7d96
Veto Ramirez <vetorobert3@gmail.com>
2020-07-10 19:47:25 -0700

    Finished game. Can revise to handle letters and more stuff.

--- rock_paper_scissors_game.py
+++ rock_paper_scissors_game.py
@@ -1,11 +1,22 @@
 import random
 
-# initialize varibles
+# file i/o functions for historical results
+def load_results():
+  text_file = open("history.txt", "r")
+  history = text_file.read().split(",")
+  text_file.close()
+  return history
+
+def save_results(w, t, l):
+  text_file = open("history.txt", "w")
+  text_file.write(str(w) + "," + str(t) + "," + str(l))
+  text_file.close() 
 
 # Welcome message
-wins = 0
-ties = 0
-losses = 0
+results = load_results()
+wins = int(results[0])
+ties = int(results[1])
+losses = int(results[2])
 print("Welcome to Rock, Paper, Scissors!")
 print("Wins: %s, Ties: %s, Losses: %s" % (wins, ties, losses))
 print("Please choose to continue...")
@@ -51,6 +62,8 @@ while not user == 9:
     else:
       print("Computer chose scissors...tie!")
       ties += 1
+  else:
+    print("Invalid selection. Please try again.")
   # print updated stats
   print("Wins: %s, Ties: %s, Losses: %s" % (wins, ties, losses))
 
@@ -59,3 +72,6 @@ while not user == 9:
   # initialize user, computer choices
   computer = random.randint(1, 3)
   user = int(input("[1] Rock [2] Paper [3] Scissors [9] Quit\n"))
+
+# game over, save results
+save_results(wins, ties, losses)
